0aa9f25b95007f4ad0e40d1115ed3d22c0e251e4
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-11-24 15:16:52 +1100

    Remove old test project and pods

---
+++ AESCryptoTests/AESCryptoTests.m
@@ -0,0 +1,34 @@
+//
+//  AESCryptoTests.m
+//  AESCryptoTests
+//
+//  Created by Evgenii Neumerzhitckii on 24/11/2013.
+//  Copyright (c) 2013 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AESCryptoTests : XCTestCase
+
+@end
+
+@implementation AESCryptoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  
+}
+
+@end
