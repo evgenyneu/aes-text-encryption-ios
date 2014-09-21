e5249f50bd4d4878056d3adf8fc3dfcae8e20fa6
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-09-21 14:01:35 +1000

    Migrating tests from specta

--- AESCryptoTests/EncryptorTests.m
+++ AESCryptoTests/EncryptorTests.m
@@ -1,6 +1,5 @@
 //
-//  AESCryptoTests.m
-//  AESCryptoTests
+//  EncryptorTests.m
 //
 //  Created by Evgenii Neumerzhitckii on 24/11/2013.
 //  Copyright (c) 2013 Evgenii Neumerzhitckii. All rights reserved.
@@ -9,11 +8,11 @@
 #import <XCTest/XCTest.h>
 #import "AESEncryptor.h"
 
-@interface AESCryptoTests : XCTestCase
+@interface EncryptorTests : XCTestCase
 
 @end
 
-@implementation AESCryptoTests
+@implementation EncryptorTests
 
 AESEncryptor *encryptor;
 
--- AESCryptoTests/TextViewDelegateTests.m
+++ AESCryptoTests/TextViewDelegateTests.m
@@ -1,18 +1,25 @@
-//#import "TestIncludes.h"
-//#import "TextViewDelegate.h"
-//
-//SpecBegin(TextViewDelegateTests)
-//
-//describe(@"get text", ^{
-//  it(@"returns text", ^{
-//    NSString *result = [TextViewDelegate text: @"this is text"];
-//    expect(result).to.equal(@"this is text");
-//  });
-//
-//  it(@"returns empty string when placeholder text is supplied", ^{
-//    NSString *result = [TextViewDelegate text: @"Message"];
-//    expect(result).to.equal(@"");
-//  });
-//});
-//
-//SpecEnd
+#import <XCTest/XCTest.h>
+#import "TextViewDelegate.h"
+
+@interface TextViewDelegateTests : XCTestCase
+
+@end
+
+@implementation TextViewDelegateTests
+
+// Get text
+// ---------
+
+- (void)testReturnText
+{
+  NSString *result = [TextViewDelegate text: @"this is text"];
+  XCTAssertEqualObjects(result, @"this is text");
+}
+
+- (void)testReturnEmptyStringWhenPlaceholderTextIsSupplied
+{
+  NSString *result = [TextViewDelegate text: @"Message"];
+  XCTAssertEqualObjects(result, @"");
+}
+
+@end
